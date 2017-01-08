7d2d9301a39296aaaf46177b7c192e29cde81dcf
Michael Yatco <mjyatco@gmail.com>
2017-01-08 14:10:54 -0500

    Andy added blah

--- storytime.rb
+++ storytime.rb
@@ -1,1 +1,1 @@
-I want a Shiba Inu - ever since I saw my first "doge" meme. I want to tie a red bandana around its neck and go on adventures with my shiba through the forests. Oh the adventures we will have!
+I want a Shiba Inu - ever since I saw my first "doge" meme. I want to tie a red bandana around its neck and go on adventures with my shiba through the forests. Oh the adventures we will have! blah
