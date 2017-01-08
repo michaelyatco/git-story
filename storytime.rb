ea6dafeb8a2b4fc7423d32f3304612d8d184a0b9
Michael Yatco <mjyatco@gmail.com>
2017-01-08 14:06:51 -0500

    Polished up the grammar

--- storytime.rb
+++ storytime.rb
@@ -1,1 +1,1 @@
-I always wanted a Shiba Inu. Ever since I saw my first "doge" meme. I want to tie a red bandana around its neck and go on adventures with my shiba through the forests.
+I want a Shiba Inu - ever since I saw my first "doge" meme. I want to tie a red bandana around its neck and go on adventures with my shiba through the forests. Oh the adventures we will have!
