6a32e84d8c23c8d0a08d9dad06e1fa6d6187a02e
Michael Yatco <mjyatco@gmail.com>
2017-01-08 15:20:03 -0500

    I tried to make storytime.rb more readable by putting it through multiple lines of code

--- storytime.rb
+++ storytime.rb
@@ -1,1 +1,4 @@
-I want a Shiba Inu - ever since I saw my first "doge" meme. I want to tie a red bandana around its neck and go on adventures with my shiba through the forests. Oh the adventures we will have! blah
+I want a Shiba Inu - 
+ever since I saw my first "doge" meme. 
+I want to tie a red bandana around its neck and go on adventures with my shiba through the forests. 
+Oh the adventures we will have! blah
